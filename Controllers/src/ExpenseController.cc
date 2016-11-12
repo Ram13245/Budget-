fab017f69ef2b5931ce4898510571c320a9112a6
Hillary <14he@queensu.ca>
2016-11-12 13:37:02 -0500

    added the ExpenseController.h and .cc skeletons

---
+++ Controllers/src/ExpenseController.cc
@@ -0,0 +1,19 @@
+#include "ExpenseController.h"
+
+// didn't include constructor, may not be applicable for ExpenseController class
+
+//takes the necessary parameters for creating a new Expense object & calls functions to add it to the data
+void ExpenseController::addExpense(Error& err, const float& amt, const string& cat, time_t date, const string& note = ""){
+     //TODO: write code for adding expenses
+}
+
+//takes the index of an expense to be deleted and preforms the necessary actions to delete it
+void ExpenseController::deleteExpense(Error& err, int idx){
+     //TODO: write code for deleting expenses
+}
+
+//fills references to vectors of tuples with with the info from expenses in the current period and histroical expenses
+void ExpenseController::populateExpenses(Error& err, vector<tuple<>>& current, vector<tuple<>>& historical){
+     //TODO: write code for populating expense vector
+}
+
