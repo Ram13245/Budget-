b788a3ab5afcd41a6418011bd79595a0d5bf8302
alexjjseppala <alexjjseppala@gmail.com>
2016-11-15 18:36:01 -0500

    Expense and Budget headers updated

--- Entity/inc/Budget.h
+++ Entity/inc/Budget.h
@@ -1,14 +1,31 @@
 #include <string>
-#include <time.h>
+#include "time.h"
 #include <iostream>
-#include "error.h"
+#include "Expense.h"
+#include "Category.h"
 
 using namespace std;
 
 class Budget
 {
 public:
-	Expense(Error& 
+	Budget(const float& amt, const time_t& sDate, const time_t& eDate, vector<Category>& categories);
+	float getTotalAmount() const;
+	pair<time_t,time_t> getDateRange() const;
+	vector<Category> getCategories() const;
+	map<string, vector<Expense>> getAllExpenses() const;
+	vector<Expense> getExpenses(const string& cat) const;
+	void changeTotalAmount(const float& newAmt);
+	void changeCategories(vector<Category> cats);
+	void deleteExpenses(vector<int> ids);
+	void addExpenses(vector<Expense> expenses);
+
+private:
+	float totalAmount;
+	time_t startDate;
+	time_t endDate;
+	map<string, category> categories;
+	map<string, vector<Expense>> expenses; 
 }
 
 
