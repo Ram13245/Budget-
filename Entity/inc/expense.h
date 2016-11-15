ae228c7ab14c543821d1eb9a4d3b746187935569
alexjjseppala <alexjjseppala@gmail.com>
2016-11-15 00:07:42 -0500

    expense entity header file commited

--- Entity/inc/expense.h
+++ Entity/inc/expense.h
@@ -1,3 +1,23 @@
+#include <string>
+#include <time>
+#include <iostream>
+
+using namespace std;
+
 class Expense
 {
+public:
+	Expense(Error& err, const float& amt, const string& cat, const time_t d, const string& note = "");
+	float getAmount() const;
+	string getCategory() const;
+	time_t getDate() const;
+	string getNote() const;
+
+private:
+	string category;
+	time_t date;
+	string note;
+	int id;
+	
+	friend ostream &operator<<(ostream &out);
 };
