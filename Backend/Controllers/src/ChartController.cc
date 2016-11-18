c916530f0f2cc4236269c8d6f8b8ef9ca8a3a0ec
Jonah <jonahchin7@gmail.com>
2016-11-17 19:59:03 -0500

    Small corrections to Chart Controller files

--- Backend/Controllers/src/ChartController.cc
+++ Backend/Controllers/src/ChartController.cc
@@ -1,19 +1,16 @@
-#include <map>
-#include <string>
-
 #include "ChartController.h"
 
 using namespace std;
 
-void ChartController::getBudgetPie(Error& err, map<string, float>& categories){
+void ChartController::getBudgetPie(Error& err, map<string, float>& data){
 
 }
 void ChartController::getBurndown(Error& err, vector<pair<time_t,float>>& exp){
 
 }
-void ChartController::getSpendingPie(Error& err, map(string, float>& ){
+void ChartController::getSpendingPie(Error& err, map(string, float>& data){
 
 }
-void ChartController::getBudgetBar(Error& err, map<string, float>& ){
+void ChartController::getBudgetBar(Error& err, map<string, float>& data){
 
 }
