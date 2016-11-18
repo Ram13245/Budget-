a132fed2d655e3ba90e244360f49d589f35c1ae2
Jonah <jonahchin7@gmail.com>
2016-11-17 19:06:42 -0500

    added incomplete chartController.cc

---
+++ Controllers/src/ChartController.cc
@@ -0,0 +1,19 @@
+#include <map>
+#include <string>
+
+#include "ChartController.h"
+
+using namespace std;
+
+void getBudgetPie(Error& err, map<string, float>& categories){
+
+}
+void getBurndown(Error& err, vector<pair<time_t,float>>& exp){
+
+} 
+void getSpendingPie(Error& err, map(string, float>& ){
+
+}
+void getBudgetBar(Error& err, map<string, float>& ){
+
+}
